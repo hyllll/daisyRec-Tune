4ed120ea24ee8bf65fa46fde36cc80bf11beda5d
hylll <631774934@qq.com>
2022-03-04 15:13:34 +0800

    update files

--- main.py
+++ main.py
@@ -291,92 +291,92 @@ if __name__ == '__main__':
     time_log.close()
     print('training complete')
 
-    # print('Start Calculating Metrics......')
-    # test_ucands = build_candidates_set(test_ur, total_train_ur, item_pool, candidates_num)
+    print('Start Calculating Metrics......')
+    test_ucands = build_candidates_set(test_ur, total_train_ur, item_pool, candidates_num)
 
-    # # get predict result
-    # print('')
-    # print('Generate recommend list...')
-    # print('')
-    # preds = {}
-    # if args.algo_name in ['vae', 'cdae', 'itemknn', 'puresvd', 'slim'] and args.problem_type == 'point':
-    #     for u in tqdm(test_ucands.keys()):
-    #         pred_rates = [model.predict(u, i) for i in test_ucands[u]]
-    #         rec_idx = np.argsort(pred_rates)[::-1][:args.topk]
-    #         top_n = np.array(test_ucands[u])[rec_idx]
-    #         preds[u] = top_n
-    # elif args.algo_name in ['mostpop']:
-    #     preds = model.predict(test_ur, total_train_ur, args.topk)
-    # else:
-    #     for u in tqdm(test_ucands.keys()):
-    #         # build a test MF dataset for certain user u to accelerate
-    #         tmp = pd.DataFrame({
-    #             'user': [u for _ in test_ucands[u]], 
-    #             'item': test_ucands[u], 
-    #             'rating': [0. for _ in test_ucands[u]], # fake label, make nonsense
-    #         })
-    #         tmp_neg_set = sampler.transform(tmp, is_training=False)
-    #         tmp_dataset = PairData(tmp_neg_set, is_training=False)
-    #         tmp_loader = data.DataLoader(
-    #             tmp_dataset,
-    #             batch_size=candidates_num, 
-    #             shuffle=False, 
-    #             num_workers=0
-    #         )
-    #         # get top-N list with torch method 
-    #         for items in tmp_loader:
-    #             user_u, item_i = items[0], items[1]
-    #             if torch.cuda.is_available():
-    #                 user_u = user_u.cuda()
-    #                 item_i = item_i.cuda()
-    #             else:
-    #                 user_u = user_u.cpu()
-    #                 item_i = item_i.cpu()
+    # get predict result
+    print('')
+    print('Generate recommend list...')
+    print('')
+    preds = {}
+    if args.algo_name in ['vae', 'cdae', 'itemknn', 'puresvd', 'slim'] and args.problem_type == 'point':
+        for u in tqdm(test_ucands.keys()):
+            pred_rates = [model.predict(u, i) for i in test_ucands[u]]
+            rec_idx = np.argsort(pred_rates)[::-1][:args.topk]
+            top_n = np.array(test_ucands[u])[rec_idx]
+            preds[u] = top_n
+    elif args.algo_name in ['mostpop']:
+        preds = model.predict(test_ur, total_train_ur, args.topk)
+    else:
+        for u in tqdm(test_ucands.keys()):
+            # build a test MF dataset for certain user u to accelerate
+            tmp = pd.DataFrame({
+                'user': [u for _ in test_ucands[u]], 
+                'item': test_ucands[u], 
+                'rating': [0. for _ in test_ucands[u]], # fake label, make nonsense
+            })
+            tmp_neg_set = sampler.transform(tmp, is_training=False)
+            tmp_dataset = PairData(tmp_neg_set, is_training=False)
+            tmp_loader = data.DataLoader(
+                tmp_dataset,
+                batch_size=candidates_num, 
+                shuffle=False, 
+                num_workers=0
+            )
+            # get top-N list with torch method 
+            for items in tmp_loader:
+                user_u, item_i = items[0], items[1]
+                if torch.cuda.is_available():
+                    user_u = user_u.cuda()
+                    item_i = item_i.cuda()
+                else:
+                    user_u = user_u.cpu()
+                    item_i = item_i.cpu()
 
-    #             prediction = model.predict(user_u, item_i)
-    #             _, indices = torch.topk(prediction, args.topk)
-    #             top_n = torch.take(torch.tensor(test_ucands[u]), indices).cpu().numpy()
+                prediction = model.predict(user_u, item_i)
+                _, indices = torch.topk(prediction, args.topk)
+                top_n = torch.take(torch.tensor(test_ucands[u]), indices).cpu().numpy()
 
-    #         preds[u] = top_n
+            preds[u] = top_n
 
-    # # convert rank list to binary-interaction
-    # for u in preds.keys():
-    #     preds[u] = [1 if i in test_ur[u] else 0 for i in preds[u]]
+    # convert rank list to binary-interaction
+    for u in preds.keys():
+        preds[u] = [1 if i in test_ur[u] else 0 for i in preds[u]]
 
-    # # process topN list and store result for reporting KPI
-    # print('Save metric@k result to res folder...')
-    # result_save_path = f'./res/{args.dataset}/{args.prepro}/{args.test_method}/'
-    # if not os.path.exists(result_save_path):
-    #     os.makedirs(result_save_path)
+    # process topN list and store result for reporting KPI
+    print('Save metric@k result to res folder...')
+    result_save_path = f'./res/{args.dataset}/{args.prepro}/{args.test_method}/'
+    if not os.path.exists(result_save_path):
+        os.makedirs(result_save_path)
 
-    # res = pd.DataFrame({'metric@K': ['pre', 'rec', 'hr', 'map', 'mrr', 'ndcg']})
-    # for k in [1, 5, 10, 20, 30, 50]:
-    #     if k > args.topk:
-    #         continue
-    #     tmp_preds = preds.copy()        
-    #     tmp_preds = {key: rank_list[:k] for key, rank_list in tmp_preds.items()}
+    res = pd.DataFrame({'metric@K': ['pre', 'rec', 'hr', 'map', 'mrr', 'ndcg']})
+    for k in [1, 5, 10, 20, 30, 50]:
+        if k > args.topk:
+            continue
+        tmp_preds = preds.copy()        
+        tmp_preds = {key: rank_list[:k] for key, rank_list in tmp_preds.items()}
 
-    #     pre_k = np.mean([precision_at_k(r, k) for r in tmp_preds.values()])
-    #     rec_k = recall_at_k(tmp_preds, test_ur, k)
-    #     hr_k = hr_at_k(tmp_preds, test_ur)
-    #     map_k = map_at_k(tmp_preds.values())
-    #     mrr_k = mrr_at_k(tmp_preds, k)
-    #     ndcg_k = np.mean([ndcg_at_k(r, k) for r in tmp_preds.values()])
+        pre_k = np.mean([precision_at_k(r, k) for r in tmp_preds.values()])
+        rec_k = recall_at_k(tmp_preds, test_ur, k)
+        hr_k = hr_at_k(tmp_preds, test_ur)
+        map_k = map_at_k(tmp_preds.values())
+        mrr_k = mrr_at_k(tmp_preds, k)
+        ndcg_k = np.mean([ndcg_at_k(r, k) for r in tmp_preds.values()])
 
-    #     if k == 10:
-    #         print(f'Precision@{k}: {pre_k:.4f}')
-    #         print(f'Recall@{k}: {rec_k:.4f}')
-    #         print(f'HR@{k}: {hr_k:.4f}')
-    #         print(f'MAP@{k}: {map_k:.4f}')
-    #         print(f'MRR@{k}: {mrr_k:.4f}')
-    #         print(f'NDCG@{k}: {ndcg_k:.4f}')
+        if k == 10:
+            print(f'Precision@{k}: {pre_k:.4f}')
+            print(f'Recall@{k}: {rec_k:.4f}')
+            print(f'HR@{k}: {hr_k:.4f}')
+            print(f'MAP@{k}: {map_k:.4f}')
+            print(f'MRR@{k}: {mrr_k:.4f}')
+            print(f'NDCG@{k}: {ndcg_k:.4f}')
 
-    #     res[k] = np.array([pre_k, rec_k, hr_k, map_k, mrr_k, ndcg_k])
+        res[k] = np.array([pre_k, rec_k, hr_k, map_k, mrr_k, ndcg_k])
 
-    # common_prefix = f'with_{args.sample_ratio}{args.sample_method}'
-    # algo_prefix = f'{args.loss_type}_{args.problem_type}_{args.algo_name}'
+    common_prefix = f'with_{args.sample_ratio}{args.sample_method}'
+    algo_prefix = f'{args.loss_type}_{args.problem_type}_{args.algo_name}'
 
-    # res.to_csv(
-    #     f'{result_save_path}{algo_prefix}_{common_prefix}_kpi_results.csv', 
-    #     index=False
-    # )
+    res.to_csv(
+        f'{result_save_path}{algo_prefix}_{common_prefix}_kpi_results.csv', 
+        index=False
+    )
