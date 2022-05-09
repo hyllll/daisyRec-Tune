9efda3028aef414e20d7d0dcf5d7d1d21140a6e4
hylll <631774934@qq.com>
2022-05-09 16:48:01 +0800

    add vae_origin

---
+++ daisy/model/VAERecommender_origin.py
@@ -0,0 +1,268 @@
+import os
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+class MatrixGenerator:
+    def __init__(self, input_matrix, batch_size=32, shuffle=True, device=None):
+        super().__init__()
+        self.input_matrix = input_matrix
+        self._num_data = self.input_matrix.shape[0]
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.device = device
+    
+    def __len__(self):
+        return int(np.ceil(self._num_data / self.batch_size))
+
+    def __iter__(self):
+        if self.shuffle:
+            perm = np.random.permutation(self._num_data)
+        else:
+            perm = np.arange(self._num_data, dtype=np.int32)
+
+        for b, st in enumerate(range(0, self._num_data, self.batch_size)):
+            ed = min(st + self.batch_size, self._num_data)
+            batch_idx = perm[st:ed]
+            
+            batch_input = torch.tensor(self.input_matrix[batch_idx].toarray(),
+                                            dtype=torch.float32, device=self.device)
+            
+            yield batch_input
+
+
+class VAE(nn.Module):
+    def __init__(self,
+                 train_data,
+                 rating_mat,
+                 q_dims=None,
+                 q=0.5,
+                 epochs=10,
+                 lr=1e-3,
+                 reg_1=0.,
+                 reg_2=0.,
+                 beta=0.5,
+                 loss_type='CL',
+                 gpuid='0',
+                 device='gpu',
+                 early_stop=True,
+                 optimizer='adam',
+                 initializer='xavier_normal'):
+        """
+        VAE Recommender Class
+        Parameters
+        ----------
+        rating_mat : np.matrix,
+        q_dims : List, Q-net dimension list
+        q : float, drop out rate
+        epochs : int, number of training epochs
+        lr : float, learning rate
+        reg_1 : float, first-order regularization term
+        reg_2 : float, second-order regularization term
+        loss_type : str, loss function type
+        gpuid : str, GPU ID
+        early_stop : bool, whether to activate early stop mechanism
+        """
+        super(VAE, self).__init__()
+
+        self.epochs = epochs
+        self.lr = lr
+        self.reg_1 = reg_1
+        self.reg_2 = reg_2
+        self.beta = beta
+        self.loss_type = loss_type
+        self.early_stop = early_stop
+        self.device = device
+        self.optimizer = optimizer
+        self.initializer = initializer
+
+        if torch.cuda.is_available() and self.device == 'gpu':
+            os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+            os.environ['CUDA_VISIBLE_DEVICES'] = str(gpuid)
+        # torch.cuda.set_device(int(gpuid)) # if internal error, try this code instead
+
+        cudnn.benchmark = True
+
+        self.train_data = train_data
+        user_num, item_num = rating_mat.shape
+        self.user_num = user_num
+        self.item_num = item_num
+        self.rating_mat = rating_mat
+
+        p_dims = [200, 600, item_num]
+        self.p_dims = p_dims
+        if q_dims:
+            assert q_dims[0] == p_dims[-1], "In and Out dimensions must equal to each other"
+            assert q_dims[-1] == p_dims[0], "Latent dimension for p- and q- network mismatches."
+            self.q_dims = q_dims
+        else:
+            self.q_dims = p_dims[::-1]
+        
+        # Last dimension of q- network is for mean and variance
+        tmp_q_dims = self.q_dims[:-1] + [self.q_dims[-1] * 2]  # essay setting only focus on 2 encoder
+        self.q_layers = nn.ModuleList(
+            [nn.Linear(d_in, d_out) for d_in, d_out in zip(tmp_q_dims[:-1], tmp_q_dims[1:])]
+        )
+        self.p_layers = nn.ModuleList(
+            [nn.Linear(d_in, d_out) for d_in, d_out in zip(self.p_dims[:-1], self.p_dims[1:])]
+        )
+        self.drop = nn.Dropout(q)
+        self._init_weights()
+
+        self.prediction = None
+
+    def _init_weights(self):
+
+        for layer in self.q_layers:
+            # Xavier Initialization for weights
+            if self.initializer == 'xavier_normal':
+                size = layer.weight.size()
+                fan_out = size[0]
+                fan_in = size[1]
+                std = np.sqrt(2.0/(fan_in + fan_out))
+                layer.weight.data.normal_(0.0, std)
+            elif self.initializer == 'xavier_uniform':
+                nn.init.xavier_uniform_(layer.weight)
+            # Normal Initialization for Biases
+            layer.bias.data.normal_(0.0, 0.001)
+        
+        for layer in self.p_layers:
+            # Xavier Initialization for weights
+            if self.initializer == 'xavier_normal':
+                size = layer.weight.size()
+                fan_out = size[0]
+                fan_in = size[1]
+                std = np.sqrt(2.0/(fan_in + fan_out))
+                layer.weight.data.normal_(0.0, std)
+            elif self.initializer == 'xavier_uniform':
+                nn.init.xavier_uniform_(layer.weight)
+
+            # Normal Initialization for Biases
+            layer.bias.data.normal_(0.0, 0.001)
+    
+    def encode(self, input):
+        h = F.normalize(input)
+        h = self.drop(h)
+        for i, layer in enumerate(self.q_layers):
+            h = layer(h)
+            if i != len(self.q_layers) - 1:
+                h = F.tanh(h)
+            else:
+                mu = h[:, :self.q_dims[-1]]  
+                logvar = h[:, self.q_dims[-1]:]
+        
+        return mu, logvar
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = torch.exp(0.5 * logvar)
+            eps = torch.randn_like(std)
+            return eps.mul(std).add_(mu)  # core calculation for predicting the real distribution
+        else:
+            return mu
+
+    def decode(self, z):
+        h = z
+        for i, layer in enumerate(self.p_layers):
+            h = layer(h)
+            if i != len(self.p_layers) - 1:
+                h = F.tanh(h)
+        return h
+
+    def forward(self, input):
+        mu, logvar = self.encode(input)
+        z = self.reparameterize(mu, logvar)
+        z = self.decode(z)
+        return z, mu, logvar
+
+    def fit(self, batch_size):
+        if torch.cuda.is_available() and self.device=='gpu':
+            self.cuda()
+        else:
+            self.cpu()
+
+        if self.loss_type == 'CL':
+            criterion = nn.BCEWithLogitsLoss(reduction='mean')
+        elif self.loss_type == 'SL':
+            criterion = nn.MSELoss(reduction='mean')
+        else:
+            raise ValueError('Invalid loss type')
+
+        if self.optimizer == 'adam':
+            optimizer = optim.Adam(self.parameters(), lr=self.lr)
+        elif self.optimizer == 'sgd':
+            optimizer = optim.SGD(self.parameters(), lr=self.lr)
+
+        last_loss = 0.
+        train_matrix = self.train_data
+        num_training = train_matrix.shape[0]
+        num_batches = int(np.ceil(num_training / batch_size))
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        batch_generator = MatrixGenerator(train_matrix, batch_size=batch_size, shuffle=True, device=device)
+
+        for epoch in range(1, self.epochs + 1):
+            self.train()
+            current_loss = 0.
+            pbar = tqdm(batch_generator)
+            pbar.set_description(f'[Epoch {epoch:03d}]')
+            for _, batch_matrix in pbar:
+                self.zero_grad()
+                pred, mu, logvar = self.forward(batch_matrix)
+                loss = criterion(pred * batch_matrix, batch_matrix)
+                KLD = -self.beta * torch.mean(torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=1))
+                loss += KLD
+
+                for layer in self.q_layers:
+                    loss += self.reg_1 * layer.weight.norm(p=1)
+                    loss += self.reg_2 * layer.weight.norm()
+                for layer in self.p_layers:
+                    loss += self.reg_1 * layer.weight.norm(p=1)
+                    loss += self.reg_2 * layer.weight.norm()
+
+                if torch.isnan(loss):
+                    raise ValueError(f'Loss=Nan or Infinity: current settings does not fit the recommender')
+                
+                loss.backward()
+                optimizer.step()
+                
+                pbar.set_postfix(loss=loss.item())
+                current_loss += loss.item()
+
+            self.eval()
+            delta_loss = float(current_loss - last_loss)
+            if (abs(delta_loss) < 1e-5) and self.early_stop:
+                print('Satisfy early stop mechanism')
+                break
+            else:
+                last_loss = current_loss
+
+
+        
+        # since there is not enough GPU memory to calculate, so we divide the data into batches, and then calculate them.
+        row_size = self.rating_mat.shape[0]
+        row_batch_size = 2048 #100
+        for i in range(row_size // row_batch_size + 1):
+            tmp = self.rating_mat[i * row_batch_size : (i + 1) * row_batch_size, :].A.squeeze()
+            tmp = torch.tensor(tmp).float()
+
+            if torch.cuda.is_available() and self.device=='gpu':
+                tmp = tmp.cuda()
+            else:
+                tmp = tmp.cpu()
+            tmp_pred = self.forward(tmp)[0]
+            tmp_pred.clamp_(min=0, max=5)
+            tmp_pred = tmp_pred.cpu().detach().numpy()
+
+            if i == 0:
+                self.prediction = tmp_pred
+            else:
+                self.prediction = np.vstack((self.prediction, tmp_pred))
+
+
+    def predict(self, u, i):
+        return self.prediction[u, i]
---
+++ vae_main.py
@@ -0,0 +1,201 @@
+import os
+import time
+import argparse
+import numpy as np
+import pandas as pd
+import scipy as sp
+from tqdm import tqdm
+from collections import defaultdict
+import sys
+
+import torch
+import torch.utils.data as data
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from daisy.utils.sampler import Sampler
+from daisy.utils.parser import parse_args
+from daisy.utils.splitter import split_test
+from daisy.utils.data import PointData, PairData, UAEData
+from daisy.utils.loader import load_rate, get_ur, convert_npy_mat, get_adj_mat, build_candidates_set
+from daisy.utils.metrics import precision_at_k, recall_at_k, map_at_k, hr_at_k, ndcg_at_k, mrr_at_k
+
+def df_to_sparse(df, shape):
+    users = df.user
+    items = df.item
+    ratings = df.rating
+
+    sp_matrix = sp.csr_matrix((ratings, (users, items)), shape=shape)
+    return sp_matrix
+
+
+if __name__ == '__main__':
+    ''' all parameter part '''
+    args = parse_args()
+
+    # store running time in time_log file
+    result_save_path = './res/time_log_jupyter/'
+    if not os.path.exists(result_save_path):
+        os.makedirs(result_save_path)
+    time_file = f'{args.dataset}_{args.prepro}_{args.test_method}_{args.problem_type}_{args.algo_name}_{args.loss_type}'
+    time_log = open(f'./res/time_log_jupyter/{time_file}.txt', 'a') 
+    if args.early_stop == 1:
+        early_stop = True
+    else:
+        early_stop = False
+    ''' Test Process for Metrics Exporting '''
+    train_set = pd.read_csv(f'./experiment_datasets/train_{args.dataset}_{args.prepro}_{args.test_method}.dat')
+    test_set = pd.read_csv(f'./experiment_datasets/test_{args.dataset}_{args.prepro}_{args.test_method}.dat')
+    if args.dataset in ['yelp']:
+        train_set['timestamp'] = pd.to_datetime(train_set['timestamp'])
+        test_set['timestamp'] = pd.to_datetime(test_set['timestamp'])
+    df = pd.concat([train_set, test_set], ignore_index=True)
+    user_num = df['user'].nunique()
+    item_num = df['item'].nunique()
+
+    train_set['rating'] = 1.0
+    test_set['rating'] = 1.0
+
+    # get ground truth
+    test_ur = get_ur(test_set)
+    total_train_ur = get_ur(train_set)
+    # initial candidate item pool
+    item_pool = set(range(item_num))
+    candidates_num = args.cand_num
+
+    print('='*50, '\n')
+    # train_dataset = UAEData(user_num, item_num, train_set, test_set)
+    train_matrix = df_to_sparse(train_set, (user_num, item_num))
+    training_mat = convert_npy_mat(user_num, item_num, train_set)
+
+    from daisy.model.VAERecommender_origin import VAE
+    model = VAE(
+            train_data=train_matrix,
+            rating_mat=training_mat,
+            q=args.dropout,
+            epochs=args.epochs,
+            lr=args.lr,
+            reg_1=args.reg_1,
+            reg_2=args.reg_2,
+            beta=args.kl_reg,
+            loss_type=args.loss_type,
+            gpuid=args.gpu,
+            device=args.device,
+            early_stop=early_stop,
+            optimizer=args.optimizer,
+            initializer=args.initializer
+        )
+     
+    # build recommender model
+    s_time = time.time()
+    # train_loader = data.DataLoader(
+    #     train_dataset, 
+    #     batch_size=args.batch_size, 
+    #     shuffle=True, 
+    #     num_workers=8,
+    #     pin_memory=True,)
+
+    model.fit()
+
+    elapsed_time = time.time() - s_time
+    time_log.write(f'{args.dataset}_{args.prepro}_{args.test_method}_{args.problem_type}_{args.algo_name}_{args.loss_type}_{args.sample_method},{elapsed_time:.4f}' + '\n')
+    time_log.close()
+    print('training complete')
+    if args.test_time == 1:
+        sys.exit(0)
+
+    print('Start Calculating Metrics......')
+    test_ucands = build_candidates_set(test_ur, total_train_ur, item_pool, candidates_num)
+
+    # get predict result
+    print('')
+    print('Generate recommend list...')
+    print('')
+    preds = {}
+    if args.algo_name in ['vae', 'cdae', 'itemknn', 'puresvd', 'slim'] and args.problem_type == 'point':
+        for u in tqdm(test_ucands.keys()):
+            pred_rates = [model.predict(u, i) for i in test_ucands[u]]
+            rec_idx = np.argsort(pred_rates)[::-1][:args.topk]
+            top_n = np.array(test_ucands[u])[rec_idx]
+            preds[u] = top_n
+    
+
+    # convert rank list to binary-interaction
+    for u in preds.keys():
+        preds[u] = [1 if i in test_ur[u] else 0 for i in preds[u]]
+
+    # process topN list and store result for reporting KPI
+    print('Save metric@k result to res folder...')
+    result_save_path = f'./res/{args.dataset}/{args.prepro}/{args.test_method}/'
+    if not os.path.exists(result_save_path):
+        os.makedirs(result_save_path)
+
+    res = pd.DataFrame({'metric@K': ['pre', 'rec', 'hr', 'map', 'mrr', 'ndcg']})
+    for k in [1, 5, 10, 20, 30, 50]:
+        if k > args.topk:
+            continue
+        tmp_preds = preds.copy()        
+        tmp_preds = {key: rank_list[:k] for key, rank_list in tmp_preds.items()}
+
+        pre_k = np.mean([precision_at_k(r, k) for r in tmp_preds.values()])
+        rec_k = recall_at_k(tmp_preds, test_ur, k)
+        hr_k = hr_at_k(tmp_preds, test_ur)
+        map_k = map_at_k(tmp_preds.values())
+        mrr_k = mrr_at_k(tmp_preds, k)
+        ndcg_k = np.mean([ndcg_at_k(r, k) for r in tmp_preds.values()])
+
+        if k == 10:
+            print(f'Precision@{k}: {pre_k:.4f}')
+            print(f'Recall@{k}: {rec_k:.4f}')
+            print(f'HR@{k}: {hr_k:.4f}')
+            print(f'MAP@{k}: {map_k:.4f}')
+            print(f'MRR@{k}: {mrr_k:.4f}')
+            print(f'NDCG@{k}: {ndcg_k:.4f}')
+
+        res[k] = np.array([pre_k, rec_k, hr_k, map_k, mrr_k, ndcg_k])
+
+    # common_prefix = f'with_{args.sample_ratio}{args.sample_method}'
+    if args.reg_2 != 0:
+        reg = 1
+    else:
+        reg = 0
+
+    if args.dropout != 0:
+        dropout = 1
+    else:
+        dropout = 0   
+
+    if args.mess_dropout != 0:
+        mess_dropout = 1
+    else:
+        mess_dropout = 0
+
+    if args.node_dropout != 0:
+        node_dropout = 1
+    else:
+        node_dropout = 0
+
+    if args.kl_reg != 0:
+        kl_reg  = 1
+    else:
+        kl_reg  = 0
+
+    if args.algo_name == 'mf' or args.algo_name == 'fm':
+        common_prefix = f'with_{reg}_{args.early_stop}'
+    elif args.algo_name == 'neumf' or args.algo_name == 'nfm':
+        common_prefix = f'with_{reg}_{dropout}_{args.early_stop}'
+    elif args.algo_name == 'ngcf':
+        common_prefix = f'with_{reg}_{node_dropout}_{mess_dropout}_{args.early_stop}'
+    elif args.algo_name == 'vae':
+        common_prefix = f'with_{reg}_{kl_reg}_{dropout}_{args.early_stop}'
+    else:
+        common_prefix = f'with'
+    
+
+    algo_prefix = f'{args.loss_type}_{args.problem_type}_{args.algo_name}'
+
+    res.to_csv(
+        f'{result_save_path}{algo_prefix}_{common_prefix}_{args.optimizer}_{args.initializer}_{args.sample_method}_{args.sample_ratio}results.csv', 
+        index=False
+    )
